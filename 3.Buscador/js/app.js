f69014cf3e7098a876b2ef8ef8434f305e61843a
Alejandro de la Rosa <alejandro.ds92@gmail.com>
2025-02-26 23:47:12 -0700

    practiceBuscar

---
+++ 3.Buscador/js/app.js
@@ -0,0 +1,23 @@
+// Variables
+const resultado = document.querySelector("#resultado");
+
+// Eventos
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarAutos();
+});
+
+// Funciones
+function mostrarAutos() {
+  autos.forEach((auto) => {
+    const { marca, modelo, year, puertas, transmision, precio, color } = auto;
+
+    const autoHTML = document.createElement("P");
+    autoHTML.textContent = `
+        ${marca}
+        ${modelo} - ${year} - ${puertas} - Transmisión:${transmision} - Precio:${precio} - Color:${color}
+    `;
+
+    // Insertar HTML
+    resultado.appendChild(autoHTML);
+  });
+}
